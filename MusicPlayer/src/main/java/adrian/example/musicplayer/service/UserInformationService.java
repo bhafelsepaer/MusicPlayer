be8561ef8df10bc3a9bfbdcebde37c663d4cd968
Adrian Durejko <dinor913@gmail.com>
2014-08-24 22:59:39 +0200

    Create interface UserInformationService

---
+++ MusicPlayer/src/main/java/adrian/example/musicplayer/service/UserInformationService.java
@@ -0,0 +1,11 @@
+package adrian.example.musicplayer.service;
+
+import java.util.List;
+
+public interface UserInformationService {
+
+	List<String> getInterest();
+	
+	List<String> getProgrammingStyle();
+		
+}
