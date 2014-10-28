4b91ad0236f67f9cf2d5cd0d2de1258dca84f673
Adrian Durejko <dinor913@gmail.com>
2014-10-28 22:50:23 +0100

    Add @OneToMany Genre

--- MusicPlayer/src/main/java/adrian/example/musicplayer/model/Music/Genres.java
+++ MusicPlayer/src/main/java/adrian/example/musicplayer/model/Music/Genres.java
@@ -1,16 +1,22 @@
 package adrian.example.musicplayer.model.Music;
 
+import java.io.Serializable;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "GENRES", uniqueConstraints = @UniqueConstraint(columnNames = { "name" }))
-public class Genres {
+public class Genres implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,6 +29,9 @@ public class Genres {
 	@Column(name = "description", length = 255, nullable = true)
 	private String genresDescription;
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "genres")
+	private Set<Track> genre_track;
+	
 	public int getGenres_id() {
 		return genres_id;
 	}
@@ -41,6 +50,14 @@ public class Genres {
 
 	public void setGenresDescription(String genresDescription) {
 		this.genresDescription = genresDescription;
+	}
+
+	public Set<Track> getGenre_track() {
+		return genre_track;
+	}
+
+	public void setGenre_track(Set<Track> genre_track) {
+		this.genre_track = genre_track;
 	}
 
 }
