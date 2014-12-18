2ab72adeb618de3a593592803b3a94caeebf8829
Adrian Durejko <dinor913@gmail.com>
2014-12-18 21:09:33 +0100

    remove unused import
    add fetch = FetchType.LAZY to @ManyToOne in Genres,Album,Artist

--- MusicPlayer/src/main/java/adrian/example/musicplayer/model/Music/Song.java
+++ MusicPlayer/src/main/java/adrian/example/musicplayer/model/Music/Song.java
@@ -1,19 +1,25 @@
 package adrian.example.musicplayer.model.Music;
 
 import java.io.Serializable;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import org.hibernate.annotations.Cascade;
+
+import adrian.example.musicplayer.model.Music.playlist.Playlist;
 
 @Entity
 @Table(name = "SONG", uniqueConstraints = 
@@ -31,17 +37,18 @@ public class Song implements Serializable {
 	@Column(name = "filename", length = 255, nullable = false)
 	private String filename;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "genre_id")
 	private Genres genres;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "album_id")
 	private Album album;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "artist_id")
 	private Artist artist;
+	
 
 	public int getSong_id() {
 		return song_id;
