65d8fb3f6fe025db26c974933fccea206eb5f380
Adrian Durejko <dinor913@gmail.com>
2014-12-18 21:11:26 +0100

    Add store in session user_id and playList

--- MusicPlayer/src/main/java/adrian/example/musicplayer/web/Controller/Welcome/WelcomeController.java
+++ MusicPlayer/src/main/java/adrian/example/musicplayer/web/Controller/Welcome/WelcomeController.java
@@ -1,16 +1,38 @@
 package adrian.example.musicplayer.web.Controller.Welcome;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import adrian.example.musicplayer.service.music.PlaylistService;
+import adrian.example.musicplayer.service.user.UserService;
+
 @Controller
 public class WelcomeController {
 	
+	@Autowired
+	UserService userService;
+	
+	@Autowired 
+	PlaylistService playListService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {
-
+	public String home(Model model, Principal principal, 
+			HttpSession session) {
+         
+		if(principal != null){
+			int user_id = userService.findLogin(principal.getName()).getUser_id();
+			session.setAttribute("user_id", user_id);
+			session.setAttribute("playlist", this.playListService.getPlaylistById(user_id));
+			
+		}
+		
 		return "home";
 	}
 }
