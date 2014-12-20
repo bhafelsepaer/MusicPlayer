3d73d36ada2ea9b49e4ebf66f8a0abddd9f1a829
Adrian Durejko <dinor913@gmail.com>
2014-12-20 19:11:02 +0100

    Add Message To all assertEquals to know what method assertEquals its call

--- MusicPlayer/src/test/java/adrian/example/musicplayer/dao/music/MusicPlayerDaoImplTest.java
+++ MusicPlayer/src/test/java/adrian/example/musicplayer/dao/music/MusicPlayerDaoImplTest.java
@@ -37,41 +37,41 @@ public class MusicPlayerDaoImplTest {
 	}
 
 	@Test
-	public void testGetSongByGenre() {
+	public void test_getSongByGenre() {
 		List<Song> loadedSong = musicPlayer.getSongByGenre("pop");
-		assertEquals("nightcore_batman", loadedSong.get(0).getName());
-		assertEquals("/location", loadedSong.get(0).getFilename());
-		assertEquals("pop", loadedSong.get(0).getGenres().getGenresName());
-		assertEquals("nice song", loadedSong.get(0).getGenres().getGenresDescription());
+		assertEquals("Tested Name", "nightcore_batman", loadedSong.get(0).getName());
+		assertEquals("Tested FileName", "/location", loadedSong.get(0).getFilename());
+		assertEquals("Tested Genres", "pop", loadedSong.get(0).getGenres().getGenresName());
+		assertEquals("Tested GenresDescription", "nice song", loadedSong.get(0).getGenres().getGenresDescription());
 	}
 	
 	@Test
-	public void testGetSongByAlbum() {
+	public void test_getSongByAlbum() {
 		List<Song> loadedSong = musicPlayer.getSongByAlbum("AlbumTest");
-		assertEquals("nightcore_batman", loadedSong.get(0).getName());
-		assertEquals("/location", loadedSong.get(0).getFilename());
-		assertEquals("AlbumTest", loadedSong.get(0).getAlbum().getAlbum_name());
-		assertEquals("/resources/a", loadedSong.get(0).getAlbum().getPicture_metadate());
-		assertEquals(2012, loadedSong.get(0).getAlbum().getRelease_date());
+		assertEquals("Tested Name", "nightcore_batman", loadedSong.get(0).getName());
+		assertEquals("Tested FileName", "/location", loadedSong.get(0).getFilename());
+		assertEquals("Tested Album Name", "AlbumTest", loadedSong.get(0).getAlbum().getAlbum_name());
+		assertEquals("Tested Metadate", "/resources/a", loadedSong.get(0).getAlbum().getPicture_metadate());
+		assertEquals("Tested Realease Date", 2012, loadedSong.get(0).getAlbum().getRelease_date());
 	}
 	
 	@Test
-	public void testGetSongByArtist() {
+	public void test_getSongByArtist() {
 		List<Song> loadedSong = musicPlayer.getSongByArtist("ArtistTest");
-		assertEquals("nightcore_batman", loadedSong.get(0).getName());
-		assertEquals("/location", loadedSong.get(0).getFilename());
-		assertEquals("ArtistTest", loadedSong.get(0).getArtist().getName());
-		assertEquals("ArtistTestDescription", loadedSong.get(0).
+		assertEquals("Tested Name", "nightcore_batman", loadedSong.get(0).getName());
+		assertEquals("Tested FileName", "/location", loadedSong.get(0).getFilename());
+		assertEquals("Tested Artist Name", "ArtistTest", loadedSong.get(0).getArtist().getName());
+		assertEquals("Tested Artist Description", "ArtistTestDescription", loadedSong.get(0).
 				                              getArtist().getDescription());
-		assertEquals(2000, loadedSong.get(0).getArtist().getYears_active());
+		assertEquals("Tested Realse Date", 2000, loadedSong.get(0).getArtist().getYears_active());
 	}
 	
 	@Test 
 	public void testLoadSongById() {
 		Song loadedSong = this.musicPlayer.loadSongById(1);
-		assertEquals(1, loadedSong.getSong_id());
-		assertEquals("nightcore_batman", loadedSong.getName());
-		assertEquals("/location", loadedSong.getFilename());
+		assertEquals("Tested Song ID", 1, loadedSong.getSong_id());
+		assertEquals("Tested  Name", "nightcore_batman", loadedSong.getName());
+		assertEquals("Tested FileName", "/location", loadedSong.getFilename());
 	}
 
 }
