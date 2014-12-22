6de297209ee63dce1294f4c70c9fe4757c2ff3fa
Adrian Durejko <dinor913@gmail.com>
2014-12-22 22:42:47 +0100

    Add javax annotation Transactional responsible for commit and rollback transactions
    Add spring annotation @TransactionalConfiguration responsible for
    configuring transactional test.
    
    In method setUp Add create new user and save him to database, this user
    will be used to test behaviour WelcomeController method.
    
    Create method "test_homePrincipallNotNull" who test url: "/" who is set
    in method "home" located in WelcomeController. This method test
    behaviour when method have parameter Principal not null.
    In this method it is created Principal with login: "TestLogin" and
    authorites: "ROLE_USER". It tested behavior of response from method
    "home", forwardedUrl what is requested for method "home", name of view
    what is accessed, test if the response status isOk(200).. 
    
    Create method "test_homePrincipallNull" who test url: "/" who is set in
    method "home" located in WelcomeController. This method test behaviour
    when method have parameter Principal null. it tested -  forwardedUrl
    what is requested for method "home", name of view what is accessed, test
    if the response status isOk(200).

--- MusicPlayer/src/test/java/adrian/example/musicplayer/web/Controller/Welcome/WelcomeControllerTest.java
+++ MusicPlayer/src/test/java/adrian/example/musicplayer/web/Controller/Welcome/WelcomeControllerTest.java
@@ -5,34 +5,75 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+import javax.transaction.Transactional;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import adrian.example.musicplayer.service.music.PlaylistService;
+import adrian.example.musicplayer.service.user.UserServiceImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 @WebAppConfiguration
+@Transactional
+@TransactionConfiguration
 public class WelcomeControllerTest {
 
 	@Autowired
 	private WebApplicationContext applicationContext;
 	
+	@Autowired
+	UserServiceImpl userService;
+	
+	@Autowired
+	PlaylistService playListService;
+	
 	private MockMvc mockMvc;
 	
 	@Before
 	public void setUp() throws Exception {
-		this.mockMvc = webAppContextSetup(this.applicationContext).alwaysExpect(status().isOk()).build();
+		this.mockMvc = webAppContextSetup(this.applicationContext).build();
+		
+		adrian.example.musicplayer.model.User.User userAfter = new adrian.example.musicplayer.model.User.User();
+		userAfter.setLogin("TestUser");
+		userAfter.setPassword("qwerty1@A");
+		userAfter.setFirstName("Adrian");
+		userAfter.setAddress("Nowa Street666");
+		userAfter.setEmail("Adrian@gmail.com");
+		this.userService.saveUser(userAfter);
 	}
 
 	@Test
-	public void test() throws Exception {
+	public void test_homePrincipallNotNull() throws Exception {
+		User user = new User("TestUser", "", AuthorityUtils.createAuthorityList("ROLE_USER"));
+		Authentication authentication = 
+				new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+		
+		int user_id =  this.userService.findLogin(authentication.getName()).getUser_id();
+		
+		this.mockMvc.perform(get("/").principal(authentication))
+		            .andExpect(view().name(containsString("home")))
+		            .andExpect(request().sessionAttribute("user_id", user_id))
+		            .andExpect(request().sessionAttribute("playlist", this.playListService.getPlaylistById(user_id)))
+		            .andExpect(forwardedUrl("/WEB-INF/views/home.jsp"))
+		            .andExpect(status().isOk());
+	}
+	
+	@Test
+	public void test_homePrincipallNull() throws Exception {
 		this.mockMvc.perform(get("/"))
 		            .andExpect(view().name(containsString("home")))
 		            .andExpect(forwardedUrl("/WEB-INF/views/home.jsp"))
