3469e5c8548d7794fac8f70e666454ee7b075f92
Adrian Durejko <dinor913@gmail.com>
2014-12-22 21:33:31 +0100

    Remove unused parameters model from method home.

--- MusicPlayer/src/main/java/adrian/example/musicplayer/web/Controller/Welcome/WelcomeController.java
+++ MusicPlayer/src/main/java/adrian/example/musicplayer/web/Controller/Welcome/WelcomeController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,14 +22,13 @@ public class WelcomeController {
 	PlaylistService playListService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model, Principal principal, 
+	public String home(Principal principal, 
 			HttpSession session) {
          
 		if(principal != null){
 			int user_id = userService.findLogin(principal.getName()).getUser_id();
 			session.setAttribute("user_id", user_id);
 			session.setAttribute("playlist", this.playListService.getPlaylistById(user_id));
-			
 		}
 		
 		return "home";
