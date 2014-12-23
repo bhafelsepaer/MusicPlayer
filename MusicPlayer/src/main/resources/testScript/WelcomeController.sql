ff36f7acaca7869b603c84106e6707d950270def
Adrian Durejko <dinor913@gmail.com>
2014-12-23 23:44:18 +0100

    Add sql script who will be insert data into tables user, user_information, user_role, playlist.

---
+++ MusicPlayer/src/main/resources/testScript/WelcomeController.sql
@@ -0,0 +1,7 @@
+INSERT INTO user VALUES (DEFAULT, 10716, 'TestAddress', 'TestEmail@Test.com', 1, 'TestFristName', 'TestLogin', '$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK');
+
+INSERT INTO user_information VALUES (1, 23, 'Football, Video Game, Travel', 'JAVA, JAVAEE', 'yes', 'TestSurname');
+
+INSERT INTO user_role VALUES (1, 'ROLE_USER', 'TestLogin', 1);
+
+INSERT INTO playlist VALUES (1, 'TestPlaylist', 1);
