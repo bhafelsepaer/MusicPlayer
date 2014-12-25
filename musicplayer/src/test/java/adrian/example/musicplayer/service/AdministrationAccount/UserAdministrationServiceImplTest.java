e953ef8899c58b23bb82b0a99947d22370b83575
Adrian Durejko <dinor913@gmail.com>
2014-12-25 23:08:12 +0100

    Remove from method setUp code responsible for create new User and save him to database, from now database is created by hibernate and file scriptSqlMusicPlayer is responsible for insert data to tables.

--- musicplayer/src/test/java/adrian/example/musicplayer/service/AdministrationAccount/UserAdministrationServiceImplTest.java
+++ musicplayer/src/test/java/adrian/example/musicplayer/service/AdministrationAccount/UserAdministrationServiceImplTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import adrian.example.musicplayer.dao.user.UserDao;
+import adrian.example.musicplayer.model.User.User;
 import adrian.example.musicplayer.model.User.UserInformation;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,69 +30,60 @@ public class UserAdministrationServiceImplTest {
 	UserAdministrationServiceImpl userServiceImpl;
 	
 	@Autowired
-	UserDao userDao;
-	
-	@Autowired
 	 BCryptPasswordEncoder bcryptEncoder;
-	
-	adrian.example.musicplayer.model.User.User user = new adrian.example.musicplayer.model.User.User();
-	
-	UserInformation userInformation = new UserInformation();
 	
 	@Before
 	public void setUp() throws Exception {
-		user.setActive_cod(123456);
-		user.setAddress("KrepnizerWeg");
-		user.setEmail("Mail@gmail.com");
-		user.setEnabled(false);
-		user.setFirstName("Armani");
-		user.setLogin("Armani");
-		user.setPassword("$2a$10$/y3BTr9MlLNXvQ.vs8H2buLHbAcbnx.JOnXLBUA.kJ0efP3TS6Riq");
-		
-		userInformation.setAge(23);
-		userInformation.setInterest("Football");
-		userInformation.setProgrammingSkill("JAVA");
-		userInformation.setSex("K");
-		userInformation.setSurname("Ferderk");
-		userInformation.setUser(user);
-		
-		user.setUserInformation(userInformation);
-		
-		userDao.saveUser(user);
 	}
 
 	@Test
 	public void getUser_id() {
-		assertEquals(user.getUser_id(), userServiceImpl.getUser_id("Armani"));
+		int user_id =  this.userServiceImpl.getUser_id("TestLogin");
+		assertEquals("Test User id", 1, user_id);
 	}
 	
 	@Test 
 	public void getUserInformationById() throws Exception {
-		UserInformation userInformationTest = userServiceImpl.getUserInformationById(user.getUser_id());
+       UserInformation userInformation = this.userServiceImpl.getUserInformationById(1);
 		
-		assertEquals("Test Age", userInformation.getAge(), userInformationTest.getAge());
-		assertEquals("Test Interest", userInformation.getInterest(), userInformationTest.getInterest());
-		assertEquals("Test Programming Skill", userInformation.getProgrammingSkill(), userInformationTest.getProgrammingSkill());
-		assertEquals("Test Sex", userInformation.getSex(), userInformationTest.getSex());
-		assertEquals("Test Surname", userInformation.getSurname(), userInformationTest.getSurname());
-		assertEquals("Test User", userInformation.getUser(), userInformationTest.getUser());
-		assertEquals("Test id", userInformation.getUser_information_id(), userInformationTest.getUser_information_id());
+	    assertEquals("Tested UserInformation id", 1, userInformation.getUser_information_id());
+		assertEquals("Tested UserInformation Age", 23, userInformation.getAge());
+		assertEquals("Tested UserInformation Interest", "Football, Video Game, Travel", 
+				userInformation.getInterest());
+		assertEquals("Tested UserInformation Sex" ,"yes", userInformation.getSex());
+		assertEquals("Tested UserInformation Programming Skill", 
+				 "JAVA, JAVAEE", userInformation.getProgrammingSkill());
 	}
 	
 	@Test
 	public void findLogin() throws Exception {
-		adrian.example.musicplayer.model.User.User userTest = userServiceImpl.findLogin("Armani");
+	    User user = this.userServiceImpl.findLogin("TestLogin");
 		
-		assertEquals("Test Login", user.getLogin(), userTest.getLogin());
-		assertEquals("Test id", user.getUser_id(), userTest.getUser_id());
-		assertEquals("Test UserInformation", user.getUserInformation(), userTest.getUserInformation());
+		assertEquals("Tested User id", 1, user.getUser_id());
+		assertEquals("Tested User Login", "TestLogin", user.getLogin());
+		assertEquals("Tested User Password", 
+				"$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK",
+				user.getPassword());
+		assertEquals("Tested User FirstName", "TestFirstName", user.getFirstName());
+		assertEquals("Tested User Address", "TestAddress", user.getAddress());
+		assertEquals("Tested User ActiveCode", 12345, user.getActive_cod());
 	}
 	
 	@Test
 	public void updateUserInformationSucced() throws Exception {
-		userServiceImpl.updateUserInformation(user.getUser_id(), "New York", "Kruger");
-		assertEquals("Test Address", "New York", user.getAddress());
-		assertEquals("Test FirstName", "Kruger", user.getFirstName());
+       User userBeforeUpdate = (User) this.userServiceImpl.findLogin("TestLogin");
+		
+		assertEquals("Test user Address Before Update", "TestAddress",
+				userBeforeUpdate.getAddress());
+		assertEquals("Test FirstName Before Update", "TestFirstName", 
+				userBeforeUpdate.getFirstName());
+		
+		this.userServiceImpl.updateUserInformation(1, "Wisla", "Aniki");
+		
+		User userAfterUpdate = (User) this.userServiceImpl.findLogin("TestLogin");
+		
+		assertEquals("Tested Address", "Wisla", userAfterUpdate.getAddress());
+		assertEquals("Tested FirstName", "Aniki", userAfterUpdate.getFirstName());
 	}
 	
 	@Test(expected = ObjectNotFoundException.class)
@@ -101,8 +93,18 @@ public class UserAdministrationServiceImplTest {
 	
 	@Test
 	public void updatePasswordSucced() throws Exception {
-		userServiceImpl.updatePassword(user.getUser_id(), "asdfG12#");
-		assertTrue("Test Password", bcryptEncoder.matches("asdfG12#",user.getPassword()));
+        User userBeforeUpdate = (User) this.userServiceImpl.findLogin("TestLogin");
+		
+		assertEquals("Tested Password Before Update", 
+				"$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK",
+				userBeforeUpdate.getPassword());
+		
+		this.userServiceImpl.updatePassword(userBeforeUpdate.getUser_id(), "qwertY1@");
+		
+		User userAfterUpdate = (User) this.userServiceImpl.findLogin("TestLogin");
+		
+		assertTrue("Tested Password After Update", bcryptEncoder.matches("qwertY1@",
+				userAfterUpdate.getPassword()));
 	}
 	
 	@Test(expected = ObjectNotFoundException.class)
@@ -112,8 +114,17 @@ public class UserAdministrationServiceImplTest {
 	
 	@Test
 	public void updateEmailSucced() throws Exception {
-		userServiceImpl.updateEmail(user.getUser_id(), "NewMail@gmail.com");
-		assertEquals("Test Update Mail", "NewMail@gmail.com", user.getEmail());
+        User userBeforeUpdate = (User) this.userServiceImpl.findLogin("TestLogin");
+		
+		assertEquals("Tested user email before update", "TestEmail@Test.com",
+				userBeforeUpdate.getEmail());
+		
+		this.userServiceImpl.updateEmail(userBeforeUpdate.getUser_id(), "NowyMail@gmail.com");
+		
+		User userAfterUpdate = (User) this.userServiceImpl.findLogin("TestLogin");
+		
+		assertEquals("tested user email after update", "NowyMail@gmail.com", 
+				userAfterUpdate.getEmail());
 	}
 	
 	@Test(expected = ObjectNotFoundException.class)
@@ -123,19 +134,43 @@ public class UserAdministrationServiceImplTest {
 	
 	@Test
 	public void updateOrSaveUserInformationSucced() throws Exception {
-		UserInformation userInformation = new UserInformation();
-		userInformation.setAge(55);
-		userInformation.setInterest("Eat");
-		userInformation.setProgrammingSkill("HTML");
-		userInformation.setSex("K");
-		userInformation.setSurname("Kazik");
+	User user = (User) this.userServiceImpl.findLogin("TestLogin");
 		
-		userServiceImpl.updateOrSaveUserInformation(user.getUser_id(), userInformation);
-		assertEquals("Test Age", 55, userInformation.getAge());
-		assertEquals("Test Interest", "Eat", userInformation.getInterest());
-		assertEquals("Test Programming Skill", "HTML", userInformation.getProgrammingSkill());
-		assertEquals("Test Sex", "K", userInformation.getSex());
-		assertEquals("Test Surname", "Kazik", userInformation.getSurname());
+		assertEquals("Tested user id", 1, user.getUser_id());
+		assertEquals("Tested UserInformation id", 1, 
+				user.getUserInformation().getUser_information_id());
+		assertEquals("Tested UserInformation age", 23, user.getUserInformation().getAge());
+		assertEquals("Tested UserInformation interest", "Football, Video Game, Travel", 
+				user.getUserInformation().getInterest());
+		assertEquals("Tested UserInformation ProgrammingSKill", "JAVA, JAVAEE", 
+				user.getUserInformation().getProgrammingSkill());
+		assertEquals("Tested UserInformation sex", "yes", user.getUserInformation().getSex());
+		assertEquals("Tested UserInformation Surname", "TestSurname", 
+				user.getUserInformation().getSurname());
+		
+		UserInformation userInformation2 = new UserInformation();
+		userInformation2.setAge(44);
+		userInformation2.setInterest("Game, Tennis");
+		userInformation2.setProgrammingSkill("Hibernate, JPA");
+		userInformation2.setSex("no");
+		userInformation2.setSurname("Czesław");
+		
+		
+		this.userServiceImpl.updateOrSaveUserInformation(user.getUser_id(), userInformation2);
+		
+        User userAfterUpdate = (User) this.userServiceImpl.findLogin("TestLogin");
+		
+		assertEquals("Tested user id", 1, userAfterUpdate.getUser_id());
+		assertEquals("Tested UserInformation id", 1, 
+				userAfterUpdate.getUserInformation().getUser_information_id());
+		assertEquals("Tested UserInformation age", 44, userAfterUpdate.getUserInformation().getAge());
+		assertEquals("Tested UserInformation interest", "Game, Tennis", 
+				userAfterUpdate.getUserInformation().getInterest());
+		assertEquals("Tested UserInformation ProgrammingSKill", "Hibernate, JPA", 
+				userAfterUpdate.getUserInformation().getProgrammingSkill());
+		assertEquals("Tested UserInformation sex", "no", userAfterUpdate.getUserInformation().getSex());
+		assertEquals("Tested UserInformation Surname", "Czesław", 
+				userAfterUpdate.getUserInformation().getSurname());  
 	}
 	
 	@Test(expected = java.lang.NullPointerException.class)
@@ -148,12 +183,13 @@ public class UserAdministrationServiceImplTest {
 		userInformationTest.setSurname("Kazik");
 		
 		userServiceImpl.updateOrSaveUserInformation(1000, userInformationTest);
-		
 	}
 	
 	@Test
 	public void checkUserVerifySucced() throws Exception {
-		assertTrue("Test Verify", userServiceImpl.checkUserVerify(user.getUser_id(), 123456));
+		 User user = (User) this.userServiceImpl.findLogin("TestLogin");
+		  assertTrue("Test User verify active code",
+				  this.userServiceImpl.checkUserVerify(user.getUser_id(), 12345));
 	}
 	
 	@Test(expected = ObjectNotFoundException.class)
@@ -163,13 +199,20 @@ public class UserAdministrationServiceImplTest {
 	
 	@Test
 	public void checkPassword() throws Exception {
-		assertTrue("Test Password", userServiceImpl.checkPassword(user.getUser_id(), "qwertY12#"));
+		  User user = (User) this.userServiceImpl.findLogin("TestLogin");
+		  assertTrue("Tested Password", this.userServiceImpl.checkPassword(user.getUser_id(), 
+				      "TestPassword12345@"));
 	}
 	
 	@Test
 	public void setEnabledTrue() throws Exception {
-		userServiceImpl.setEnabledTrue(user.getUser_id());
-		assertTrue(user.isEnabled());
+		  User userBeforeUpdateEnabled = (User) this.userServiceImpl.findLogin("TestLogin");
+		  assertFalse("Tested Enabled before update", userBeforeUpdateEnabled.isEnabled());
+		  
+		  this.userServiceImpl.setEnabledTrue(userBeforeUpdateEnabled.getUser_id());
+		  
+		  User userAfterUpdateEnabled = (User) this.userServiceImpl.findLogin("TestLogin");
+		  assertTrue("Tested Enabled After Update ", userAfterUpdateEnabled.isEnabled());
 	}
 	
 }
