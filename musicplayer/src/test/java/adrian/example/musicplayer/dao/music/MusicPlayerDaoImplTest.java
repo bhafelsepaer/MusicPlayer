e3018a0d89eeee6af40a7de881295af8d9b0a460
Adrian Durejko <dinor913@gmail.com>
2014-12-25 16:26:15 +0100

    Remove from method setUp code responsible for create new Song and save it from database. From now we create table Song and we insert data into tables using hibernate.
    Change all values actualValues to values what is inserted into databases

--- musicplayer/src/test/java/adrian/example/musicplayer/dao/music/MusicPlayerDaoImplTest.java
+++ musicplayer/src/test/java/adrian/example/musicplayer/dao/music/MusicPlayerDaoImplTest.java
@@ -27,51 +27,46 @@ public class MusicPlayerDaoImplTest {
 	@Autowired
 	MusicPlayerDao musicPlayer;
 	
-	Song song = new Song();
-	
 	@Before
 	public void setUp() throws Exception {
-		song.setFilename("/location");
-		song.setName("nightcore_batman");
-		musicPlayer.saveSong(song);
 	}
 
 	@Test
 	public void test_getSongByGenre() {
-		List<Song> loadedSong = musicPlayer.getSongByGenre("pop");
-		assertEquals("Tested Name", "nightcore_batman", loadedSong.get(0).getName());
-		assertEquals("Tested FileName", "/location", loadedSong.get(0).getFilename());
-		assertEquals("Tested Genres", "pop", loadedSong.get(0).getGenres().getGenresName());
-		assertEquals("Tested GenresDescription", "nice song", loadedSong.get(0).getGenres().getGenresDescription());
+		List<Song> loadedSong = musicPlayer.getSongByGenre("TestGenres");
+		assertEquals("Tested Name", "TestNameSong", loadedSong.get(0).getName());
+		assertEquals("Tested FileName", "TestFileNameSong", loadedSong.get(0).getFilename());
+		assertEquals("Tested Genres", "TestGenres", loadedSong.get(0).getGenres().getGenresName());
+		assertEquals("Tested GenresDescription", "TestDescription", loadedSong.get(0).getGenres().getGenresDescription());
 	}
 	
 	@Test
 	public void test_getSongByAlbum() {
-		List<Song> loadedSong = musicPlayer.getSongByAlbum("AlbumTest");
-		assertEquals("Tested Name", "nightcore_batman", loadedSong.get(0).getName());
-		assertEquals("Tested FileName", "/location", loadedSong.get(0).getFilename());
-		assertEquals("Tested Album Name", "AlbumTest", loadedSong.get(0).getAlbum().getAlbum_name());
-		assertEquals("Tested Metadate", "/resources/a", loadedSong.get(0).getAlbum().getPicture_metadate());
-		assertEquals("Tested Realease Date", 2012, loadedSong.get(0).getAlbum().getRelease_date());
+		List<Song> loadedSong = musicPlayer.getSongByAlbum("TestAlbum");
+		assertEquals("Tested Name", "TestNameSong", loadedSong.get(0).getName());
+		assertEquals("Tested FileName", "TestFileNameSong", loadedSong.get(0).getFilename());
+		assertEquals("Tested Album Name", "TestAlbum", loadedSong.get(0).getAlbum().getAlbum_name());
+		assertEquals("Tested Metadate", "/TestLocation", loadedSong.get(0).getAlbum().getPicture_metadate());
+		assertEquals("Tested Realease Date", 2000, loadedSong.get(0).getAlbum().getRelease_date());
 	}
 	
 	@Test
 	public void test_getSongByArtist() {
-		List<Song> loadedSong = musicPlayer.getSongByArtist("ArtistTest");
-		assertEquals("Tested Name", "nightcore_batman", loadedSong.get(0).getName());
-		assertEquals("Tested FileName", "/location", loadedSong.get(0).getFilename());
-		assertEquals("Tested Artist Name", "ArtistTest", loadedSong.get(0).getArtist().getName());
-		assertEquals("Tested Artist Description", "ArtistTestDescription", loadedSong.get(0).
+		List<Song> loadedSong = musicPlayer.getSongByArtist("TestArtist");
+		assertEquals("Tested Name", "TestNameSong", loadedSong.get(0).getName());
+		assertEquals("Tested FileName", "TestFileNameSong", loadedSong.get(0).getFilename());
+		assertEquals("Tested Artist Name", "TestArtist", loadedSong.get(0).getArtist().getName());
+		assertEquals("Tested Artist Description", "TestDescription", loadedSong.get(0).
 				                              getArtist().getDescription());
-		assertEquals("Tested Realse Date", 2000, loadedSong.get(0).getArtist().getYears_active());
+		assertEquals("Tested Realse Date", 1999, loadedSong.get(0).getArtist().getYears_active());
 	}
 	
 	@Test 
 	public void testLoadSongById() {
 		Song loadedSong = this.musicPlayer.loadSongById(1);
 		assertEquals("Tested Song ID", 1, loadedSong.getSong_id());
-		assertEquals("Tested  Name", "nightcore_batman", loadedSong.getName());
-		assertEquals("Tested FileName", "/location", loadedSong.getFilename());
+		assertEquals("Tested  Name", "TestNameSong", loadedSong.getName());
+		assertEquals("Tested FileName", "TestFileNameSong", loadedSong.getFilename());
 	}
 
 }
