c5f44f6d587a11f69e32240b452b409495337bf6
Adrian Durejko <dinor913@gmail.com>
2014-12-25 22:25:41 +0100

    Remove code from method setUp responsible for save user to database for now we use scriptSqlMusicPlayer.sql to save data to database what we use to test UserDaoImpl.class.
    In method saveUser add create new User, set data to new User and save
    him to database. After saving to database User we load him by id and
    Test all field belong to User.
    
      
    In all method who needs we correct set parameter in method to String.

--- musicplayer/src/test/java/adrian/example/musicplayer/dao/user/UserDaoImplTest.java
+++ musicplayer/src/test/java/adrian/example/musicplayer/dao/user/UserDaoImplTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import adrian.example.musicplayer.model.User.User;
 import adrian.example.musicplayer.model.User.UserInformation;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,141 +30,197 @@ public class UserDaoImplTest {
 	@Autowired
 	private BCryptPasswordEncoder bcryptEncoder;
 	
-	adrian.example.musicplayer.model.User.User user = new adrian.example.musicplayer.model.User.User();
-	UserInformation userInformation = new UserInformation();
 	
 	@Before
 	public void setUp() throws Exception {
-		user.setActive_cod(123456);
-		user.setAddress("KremnizerWeg");
-		user.setConfirmEmail("email@gmail.com");
-		user.setConfirmPassword("zxcvbnm");
-		user.setEmail("email@gmail.com");
-		user.setEnabled(false);
-		user.setFirstName("Adam");
-		user.setLogin("Adam");
-		user.setPassword("$2a$10$/y3BTr9MlLNXvQ.vs8H2buLHbAcbnx.JOnXLBUA.kJ0efP3TS6Riq");
-		userInformation.setAge(23);
-		userInformation.setInterest("Music");
-		userInformation.setProgrammingSkill("Java");
-		userInformation.setSex("M");
-		userInformation.setSurname("Kitajec");
-		
-		userInformation.setUser(user);
-		user.setUserInformation(userInformation);
-		
-		userDaoImpl.saveUser(user);
-		
 	}
 	
 	@Test
 	public void saveUser() throws Exception {
-		assertEquals("Test Active Cod", 123456, user.getActive_cod());
-		assertEquals("Tested Address", "KremnizerWeg", user.getAddress());
-		assertEquals("Tested Email", "email@gmail.com", user.getEmail());
-		assertEquals("Tested Password", "$2a$10$/y3BTr9MlLNXvQ.vs8H2buLHbAcbnx.JOnXLBUA.kJ0efP3TS6Riq", user.getPassword());
-		assertEquals("Tested Enabled", false, user.isEnabled());
-		assertEquals("Tested First Name", "Adam", user.getFirstName());
-		assertEquals("Tested Email", "email@gmail.com", user.getEmail());
-	}
+		User userToSave = new User();
+		userToSave.setLogin("User2");
+		userToSave.setActive_cod(54321);
+		userToSave.setAddress("AddressSave");
+		userToSave.setEmail("SavedEmail@Test.com");
+		userToSave.setEnabled(false);
+		userToSave.setFirstName("SavedName");
+		userToSave.setPassword("password12345@");
+		
+		this.userDaoImpl.saveUser(userToSave);
+        
+		User loadedUser = (User) this.userDaoImpl.findUserByLogin("User2");
+		
+		assertEquals("Tested User id", 2, loadedUser.getUser_id());
+		assertEquals("Tested User Login", "User2", loadedUser.getLogin());
+		assertEquals("Tested User Password", 
+				"password12345@",
+				loadedUser.getPassword());
+		assertEquals("Tested User FirstName", "SavedName", loadedUser.getFirstName());
+		assertEquals("Tested User Address", "AddressSave", loadedUser.getAddress());
+		assertEquals("Tested User ActiveCode", 54321, loadedUser.getActive_cod());
+	} 
 	
 	@Test
 	public void getUser_id() throws Exception {
-		int id = userDaoImpl.getUser_id(user.getLogin());
-		assertEquals("Tested User id" ,id, user.getUser_id());
-	}
+		int user_id = userDaoImpl.getUser_id("TestLogin");
+		
+		assertEquals("Tested User id" ,1 , user_id);
+	} 
 	
 	@Test
 	public void findLogin() throws Exception {
-		adrian.example.musicplayer.model.User.User user2 = userDaoImpl.findUserByLogin(user.getLogin());
-		assertEquals("Tested Login", user2.getLogin(), user.getLogin());
-		assertEquals("Tested Password", user2.getPassword(), user.getPassword());
-		assertEquals("Tested User id", user2.getUser_id(), user.getUser_id());
-		assertEquals("Tested FirstName", user2.getFirstName(), user.getFirstName());
-	}
+		User user = userDaoImpl.findUserByLogin("TestLogin");
+		
+		assertEquals("Tested User id", 1, user.getUser_id());
+		assertEquals("Tested User Login", "TestLogin", user.getLogin());
+		assertEquals("Tested User Password", 
+				"$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK",
+				user.getPassword());
+		assertEquals("Tested User FirstName", "TestFirstName", user.getFirstName());
+		assertEquals("Tested User Address", "TestAddress", user.getAddress());
+		assertEquals("Tested User ActiveCode", 12345, user.getActive_cod());
+	} 
 	
-	@Test
-	public void findLoginNull() throws Exception {
-		assertNull("Tested Login", userDaoImpl.findUserByLogin("Janek"));
-	}
 	
 	@Test
 	public void findUserById() throws Exception {
-		adrian.example.musicplayer.model.User.User user3 = userDaoImpl.findUserById(user.getUser_id());
-		assertEquals("Tested Login", user3.getLogin(), user.getLogin());
-		assertEquals("Tested Password", user3.getPassword(), user.getPassword());
-		assertEquals("Tested User id", user3.getUser_id(), user.getUser_id());
-		assertEquals("Tested First Name", user3.getFirstName(), user.getFirstName());
-	}
+		User user = (User) this.userDaoImpl.findUserById(1);
+		
+		assertEquals("Tested User id", 1, user.getUser_id());
+		assertEquals("Tested User Login", "TestLogin", user.getLogin());
+		assertEquals("Tested User Password", 
+				"$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK",
+				user.getPassword());
+		assertEquals("Tested User FirstName", "TestFirstName", user.getFirstName());
+		assertEquals("Tested User Address", "TestAddress", user.getAddress());
+		assertEquals("Tested User ActiveCode", 12345, user.getActive_cod());
+		
+	} 
 	
 	@Test
-	public void getUserInformationByIdExist() throws Exception {
-		UserInformation userInformation2 = userDaoImpl.getUserInformationById(user.getUser_id());
-		assertEquals("Tested Age", userInformation2.getAge(), userInformation.getAge());
-		assertEquals("Tested Interest", userInformation2.getInterest(), userInformation.getInterest());
-		assertEquals("Tested Sex" ,userInformation2.getSex(), userInformation.getSex());
-		assertEquals("Tested id", userInformation2.getUser_information_id(), userInformation.getUser_information_id());
-	}
-	
-	@Test
-	public void getUserInformationByIdNewUserInformation() throws Exception {
-		UserInformation userInformation3 = userDaoImpl.getUserInformationById(5000);
-		assertNull("Tested Interest",userInformation3.getInterest());
-		assertNull("Tested ProgrammingSkill", userInformation3.getProgrammingSkill());
-		assertNull("Tested Sex", userInformation3.getSex());
-		assertNull("Tested Surname", userInformation3.getSurname());
-		assertEquals("Tested Age", 0, userInformation3.getAge());
-	}
+	public void getUserInformationById() throws Exception {
+		UserInformation userInformation = userDaoImpl.getUserInformationById(1);
+		
+		assertEquals("Tested UserInformation id", 1, userInformation.getUser_information_id());
+		assertEquals("Tested UserInformation Age", 23, userInformation.getAge());
+		assertEquals("Tested UserInformation Interest", "Football, Video Game, Travel", 
+				userInformation.getInterest());
+		assertEquals("Tested UserInformation Sex" ,"yes", userInformation.getSex());
+		assertEquals("Tested UserInformation Programming Skill", 
+				 "JAVA, JAVAEE", userInformation.getProgrammingSkill());
+	} 
 	
 	@Test
 	public void updateUserInformation() throws Exception {
-		userDaoImpl.updateUserInformation(user.getUser_id(), "Wisla", "Aniki");
-		assertEquals("Tested Address", "Wisla", user.getAddress());
-		assertEquals("Tested FirstName", "Aniki", user.getFirstName());
-	}
+		
+		User userBeforeUpdate = (User) this.userDaoImpl.findUserById(1);
+		
+		assertEquals("Test user Address Before Update", "TestAddress",
+				userBeforeUpdate.getAddress());
+		assertEquals("Test FirstName Before Update", "TestFirstName", 
+				userBeforeUpdate.getFirstName());
+		
+		userDaoImpl.updateUserInformation(1, "Wisla", "Aniki");
+		
+		User userAfterUpdate = (User) this.userDaoImpl.findUserById(1);
+		
+		assertEquals("Tested Address", "Wisla", userAfterUpdate.getAddress());
+		assertEquals("Tested FirstName", "Aniki", userAfterUpdate.getFirstName());
+	} 
 	
 	@Test
 	public void updatePassword() throws Exception {
-		userDaoImpl.updatePassword(user.getUser_id(), "qwertY1@");
-		assertTrue("Tested Password", bcryptEncoder.matches("qwertY1@",user.getPassword()));
-	}
+		User userBeforeUpdate = (User) this.userDaoImpl.findUserById(1);
+		
+		assertEquals("Tested Password Before Update", 
+				"$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK",
+				userBeforeUpdate.getPassword());
+		
+		userDaoImpl.updatePassword(userBeforeUpdate.getUser_id(), "qwertY1@");
+		
+		User userAfterUpdate = (User) this.userDaoImpl.findUserById(1);
+		
+		assertTrue("Tested Password After Update", bcryptEncoder.matches("qwertY1@",
+				userAfterUpdate.getPassword()));
+	} 
 	
 	@Test
 	public void updateEmail() throws Exception {
-		userDaoImpl.updateEmail(user.getUser_id(), "NowyMail@gmail.com");
-		assertEquals("NowyMail@gmail.com", user.getEmail());
-	}
+		User userBeforeUpdate = (User) this.userDaoImpl.findUserById(1);
+		
+		assertEquals("Tested user email before update", "TestEmail@Test.com",
+				userBeforeUpdate.getEmail());
+		
+		userDaoImpl.updateEmail(userBeforeUpdate.getUser_id(), "NowyMail@gmail.com");
+		
+		User userAfterUpdate = (User) this.userDaoImpl.findUserById(1);
+		
+		assertEquals("tested user email after update", "NowyMail@gmail.com", 
+				userAfterUpdate.getEmail());
+	} 
 	
 	@Test
 	public void updateOrSaveInformation() throws Exception {
+        
+		User user = (User) this.userDaoImpl.findUserById(1);
+		
+		assertEquals("Tested user id", 1, user.getUser_id());
+		assertEquals("Tested UserInformation id", 1, 
+				user.getUserInformation().getUser_information_id());
+		assertEquals("Tested UserInformation age", 23, user.getUserInformation().getAge());
+		assertEquals("Tested UserInformation interest", "Football, Video Game, Travel", 
+				user.getUserInformation().getInterest());
+		assertEquals("Tested UserInformation ProgrammingSKill", "JAVA, JAVAEE", 
+				user.getUserInformation().getProgrammingSkill());
+		assertEquals("Tested UserInformation sex", "yes", user.getUserInformation().getSex());
+		assertEquals("Tested UserInformation Surname", "TestSurname", 
+				user.getUserInformation().getSurname());
+		
 		UserInformation userInformation2 = new UserInformation();
 		userInformation2.setAge(44);
-		userInformation2.setInterest("SEX");
-		userInformation2.setProgrammingSkill("Hibernate");
-		userInformation2.setSex("K");
-		userInformation2.setSurname("Stefania");
+		userInformation2.setInterest("Game, Tennis");
+		userInformation2.setProgrammingSkill("Hibernate, JPA");
+		userInformation2.setSex("no");
+		userInformation2.setSurname("Czesław");
+		
 		
 		userDaoImpl.updateOrSaveUserInformation(user.getUser_id(), userInformation2);
 		
-		assertEquals("Tested Age", userInformation.getAge(), userInformation2.getAge());
-		assertEquals("Tested Interest", userInformation.getInterest(), userInformation2.getInterest());
-		assertEquals("Tested Programming Skill", userInformation.getProgrammingSkill(), userInformation2.getProgrammingSkill());
-		assertEquals("Tested Sex", userInformation.getSex(), userInformation2.getSex());
-		assertEquals("Tested Surname", userInformation.getSurname(), userInformation2.getSurname());
-	}
+        User userAfterUpdate = (User) this.userDaoImpl.findUserById(1);
+		
+		assertEquals("Tested user id", 1, userAfterUpdate.getUser_id());
+		assertEquals("Tested UserInformation id", 1, 
+				userAfterUpdate.getUserInformation().getUser_information_id());
+		assertEquals("Tested UserInformation age", 44, userAfterUpdate.getUserInformation().getAge());
+		assertEquals("Tested UserInformation interest", "Game, Tennis", 
+				userAfterUpdate.getUserInformation().getInterest());
+		assertEquals("Tested UserInformation ProgrammingSKill", "Hibernate, JPA", 
+				userAfterUpdate.getUserInformation().getProgrammingSkill());
+		assertEquals("Tested UserInformation sex", "no", userAfterUpdate.getUserInformation().getSex());
+		assertEquals("Tested UserInformation Surname", "Czesław", 
+				userAfterUpdate.getUserInformation().getSurname());  
+	} 
   
   @Test
   public void checkPassword() throws Exception {
-	  assertTrue("Tested Password", userDaoImpl.checkPassword(user.getUser_id(), "qwertY12#"));
+	  User user = (User) this.userDaoImpl.findUserById(1);
+	  assertTrue("Tested Password", userDaoImpl.checkPassword(user.getUser_id(), 
+			      "TestPassword12345@"));
   }
   
   @Test
   public void checkUserVerify() throws Exception {
-	  assertTrue("Tested Active cod", userDaoImpl.checkUserVerify(user.getUser_id(), user.getActive_cod()));
+	  User user = (User) this.userDaoImpl.findUserById(1);
+	  assertTrue("Test User verify active code",
+			  this.userDaoImpl.checkUserVerify(user.getUser_id(), 12345));
   }
-	
+  
+  @Test
   public void setEnebledTrue() throws Exception {
+	  User user = (User) this.userDaoImpl.findUserById(1);
+	  assertFalse("Tested Enabled before update", user.isEnabled());
+	  
 	  userDaoImpl.setEnabledTrue(user.getUser_id());
-	  assertTrue("Tested Enabled", user.isEnabled());
+	  assertTrue("Tested Enabled After Update ", user.isEnabled());
   }
 }
