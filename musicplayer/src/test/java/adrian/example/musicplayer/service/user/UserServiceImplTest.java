196520e835b4b3ce2b0401e4caee2c3d6f47a439
Adrian Durejko <dinor913@gmail.com>
2014-12-26 04:22:40 +0100

    Remove code from method setUp responsible for save User to database, from now we insert data into database from sql script.
    Impoved method 'saveUser' and 'findLogin'

--- musicplayer/src/test/java/adrian/example/musicplayer/service/user/UserServiceImplTest.java
+++ musicplayer/src/test/java/adrian/example/musicplayer/service/user/UserServiceImplTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import adrian.example.musicplayer.dao.user.UserDao;
+import adrian.example.musicplayer.model.User.User;
 import adrian.example.musicplayer.model.User.UserInformation;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -25,75 +26,51 @@ import adrian.example.musicplayer.model.User.UserInformation;
 @TransactionConfiguration
 public class UserServiceImplTest {
 	
-	@Autowired
-	UserDao userDao;
-	
 	@Autowired 
 	UserServiceImpl userServiceImpl;
 	
 	@Autowired
 	BCryptPasswordEncoder passwordEncoder;
 	
-    adrian.example.musicplayer.model.User.User user = new adrian.example.musicplayer.model.User.User();
-	
-	UserInformation userInformation = new UserInformation();
-	
-	private String login = "Armani";
-	
 	@Before
-	public void setUp() throws Exception {
-		user.setActive_cod(123456);
-		user.setAddress("KrepnizerWeg");
-		user.setEmail("Mail@gmail.com");
-		user.setEnabled(false);
-		user.setFirstName("Armani");
-		user.setLogin(login);
-		user.setPassword("$2a$10$/y3BTr9MlLNXvQ.vs8H2buLHbAcbnx.JOnXLBUA.kJ0efP3TS6Riq");
-		
-		userInformation.setAge(23);
-		userInformation.setInterest("Football");
-		userInformation.setProgrammingSkill("JAVA");
-		userInformation.setSex("K");
-		userInformation.setSurname("Ferderk");
-		userInformation.setUser(user);
-		
-		user.setUserInformation(userInformation);
-		
-		userDao.saveUser(user);
-	}
+	public void setUp() throws Exception {}
 
 	@Test
 	public void findLogin() throws Exception {
-		 adrian.example.musicplayer.model.User.User user2 = userServiceImpl.findLogin(login);
-		 assertEquals("Test Address", user2.getAddress(), user.getAddress());
-		 assertEquals("Test Active Cod", user2.getActive_cod(), user.getActive_cod());
-		 assertEquals("Test Email", user2.getEmail(), user.getEmail());
-		 assertEquals("Test First Name", user2.getFirstName(), user.getFirstName());
-		 assertEquals("Test login", user2.getLogin(), user.getLogin());
-		 assertEquals("Test Password", user2.getPassword(), user.getPassword());
-		 assertEquals("Test id", user2.getUser_id(), user.getUser_id());
+        User user = this.userServiceImpl.findLogin("TestLogin");
+		
+		assertEquals("Tested User id", 1, user.getUser_id());
+		assertEquals("Tested User Login", "TestLogin", user.getLogin());
+		assertEquals("Tested User Password", 
+				"$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK",
+				user.getPassword());
+		assertEquals("Tested User FirstName", "TestFirstName", user.getFirstName());
+		assertEquals("Tested User Address", "TestAddress", user.getAddress());
+		assertEquals("Tested User ActiveCode", 12345, user.getActive_cod());
 	}
 	
 	@Test
 	public void saveUser() throws Exception {
-		adrian.example.musicplayer.model.User.User user2 = new adrian.example.musicplayer.model.User.User();
-		user2.setActive_cod(543463);
-		user2.setAddress("Vegas");
-		user2.setEmail("Maradona@gmail.com");
-		user2.setEnabled(false);
-		user2.setFirstName("Chikita");
-		user2.setLogin("Kazik666Cebulak");
-		user2.setPassword("qwertY12#");
+		User userToSave = new User();
+		userToSave.setLogin("User2");
+		userToSave.setActive_cod(54321);
+		userToSave.setAddress("AddressSave");
+		userToSave.setEmail("SavedEmail@Test.com");
+		userToSave.setEnabled(false);
+		userToSave.setFirstName("SavedName");
+		userToSave.setPassword("password12345@");
 		
-		userServiceImpl.saveUser(user2);
+		this.userServiceImpl.saveUser(userToSave);
+        
+		User loadedUser = (User) this.userServiceImpl.findLogin("User2");
 		
-		assertEquals("Test Active Cod", 543463, user2.getActive_cod());
-		assertEquals("Test Address", "Vegas", user2.getAddress());
-		assertEquals("Test email", "Maradona@gmail.com", user2.getEmail());
-		assertFalse("Test Enabled", user2.isEnabled());
-		assertEquals("Test First Name", "Chikita", user2.getFirstName());
-		assertEquals("Test Login", "Kazik666Cebulak", user2.getLogin());
-	    assertTrue("Test Correct Password", passwordEncoder.matches("qwertY12#", user2.getPassword()));
+		assertEquals("Tested User id", 2, loadedUser.getUser_id());
+		assertEquals("Tested User Login", "User2", loadedUser.getLogin());
+		assertTrue("Tested User Password", 
+				passwordEncoder.matches("password12345@", loadedUser.getPassword()));
+		assertEquals("Tested User FirstName", "SavedName", loadedUser.getFirstName());
+		assertEquals("Tested User Address", "AddressSave", loadedUser.getAddress());
+		assertEquals("Tested User ActiveCode", 54321, loadedUser.getActive_cod());
 	}
 
 }
