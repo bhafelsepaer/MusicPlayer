a42454bfd90f438178e55fd51967051c44a7f0a1
Adrian Durejko <dinor913@gmail.com>
2014-12-30 02:09:22 +0100

    Create Entity represent SQL Table playlist_song. Create Mapping with Entity Song and Playlist represent in this Entity foreign key.

---
+++ musicplayer/src/main/java/adrian/example/musicplayer/model/Music/playlist/PlaylistSong.java
@@ -0,0 +1,55 @@
+package adrian.example.musicplayer.model.Music.playlist;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import adrian.example.musicplayer.model.Music.Song;
+
+@Entity
+@Table(name = "playlist_song")
+public class PlaylistSong {
+   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "playlist_song_id")
+	private int playlistSong_id;
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "playlist_id", nullable = false)
+	private Playlist playlist;
+	
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "song_id", nullable = false)
+	private Song song;
+	
+	public int getPlaylistSong_id() {
+		return playlistSong_id;
+	}
+
+	public Song getSong() {
+		return song;
+	}
+
+	public void setSong(Song song) {
+		this.song = song;
+	}
+
+	public Playlist getPlaylist() {
+		return playlist;
+	}
+
+	public void setPlaylist(Playlist playlist) {
+		this.playlist = playlist;
+	}
+	
+	
+	
+}
