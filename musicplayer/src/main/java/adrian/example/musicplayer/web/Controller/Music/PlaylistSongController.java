421fd72f4cee5636babc3d4207ff36170dea2950
Adrian Durejko <dinor913@gmail.com>
2014-12-31 02:12:30 +0100

    Create Controller PlaylistController Responsible for all operation on song in Playlist

---
+++ musicplayer/src/main/java/adrian/example/musicplayer/web/Controller/Music/PlaylistSongController.java
@@ -0,0 +1,29 @@
+package adrian.example.musicplayer.web.Controller.Music;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import adrian.example.musicplayer.service.music.PlayListSongService;
+
+@Controller
+public class PlaylistSongController {
+	
+	@Autowired
+	PlayListSongService playListSongService;
+	
+	@RequestMapping(value  = "/saveSongToPlaylist", method = RequestMethod.GET)
+	@ResponseBody
+	public String saveSongToPlaylist(
+			@RequestParam(value = "song_id", required = true) int song_id,
+			@RequestParam(value = "playlist_id", required = true) int playlist_id){
+		
+		this.playListSongService.savePlaylistSong(song_id, playlist_id);
+		return "succed";
+	}
+
+}
