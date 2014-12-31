07b46e2c243e98d73c62e8a4ee823cc0b9bda899
Adrian Durejko <dinor913@gmail.com>
2014-12-31 02:15:39 +0100

    Mark this class as Service and add annotation @Transactional to all method in this class

--- musicplayer/src/main/java/adrian/example/musicplayer/service/music/PlayListSongServiceImpl.java
+++ musicplayer/src/main/java/adrian/example/musicplayer/service/music/PlayListSongServiceImpl.java
@@ -1,16 +1,21 @@
 package adrian.example.musicplayer.service.music;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import adrian.example.musicplayer.dao.music.PlayListSongDao;
 import adrian.example.musicplayer.model.Music.playlist.PlaylistSong;
 
+@Service
 public class PlayListSongServiceImpl implements PlayListSongService {
 
 	@Autowired
 	PlayListSongDao playlistSongDao;
 	
 	@Override
+	@Transactional
 	public void savePlaylistSong(int song_id, int playlist_id) {
 		this.playlistSongDao.
 		     savePlaylistSong(song_id, playlist_id);
@@ -18,6 +23,7 @@ public class PlayListSongServiceImpl implements PlayListSongService {
 	}
 
 	@Override
+	@Transactional
 	public PlaylistSong getPlaylistSongById(int playlistSongId) {
 		return this.playlistSongDao.
 				   getPlaylistSongById(playlistSongId);
