9fd2915134cf9f0e4069fd42d7fd431a215217f1
Adrian Durejko <dinor913@gmail.com>
2015-01-02 23:00:44 +0100

    Add "@SessionAttributes("user")" to add entity "User" to all method in this Controller.
    In method getSongPlaylist load List of "PlayListSong" and next add to
    List of "Song" all "Song" from "PlayListSong", return jsp -
    "musicplayer/playlist/showSongInPlaylist".

--- musicplayer/src/main/java/adrian/example/musicplayer/web/Controller/Music/PlaylistSongController.java
+++ musicplayer/src/main/java/adrian/example/musicplayer/web/Controller/Music/PlaylistSongController.java
@@ -1,17 +1,27 @@
 package adrian.example.musicplayer.web.Controller.Music;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import adrian.example.musicplayer.model.Music.Song;
+import adrian.example.musicplayer.model.Music.playlist.PlaylistSong;
 import adrian.example.musicplayer.service.music.PlayListSongService;
 
 @Controller
 @RequestMapping(value = "/playlist")
+@SessionAttributes("user")
 public class PlaylistSongController {
 	
 	@Autowired
@@ -27,12 +37,21 @@ public class PlaylistSongController {
 		return "succed";
 	}
 	
-	@RequestMapping(value = "/getSongPlaylist", method = RequestMethod.GET)
+	@RequestMapping(value = "/showPlaylistSong/{playlist_id}", method = RequestMethod.GET)
 	public String getSongPlaylist(
-			@RequestParam(value = "playlist_id", required = true) int  playlist_id){
+			@PathVariable(value = "playlist_id") int  playlist_id,
+			Model model){
 
-		  
-		return null;
+		List<PlaylistSong> playlistSong = 
+				(List<PlaylistSong>) this.playListSongService.getPlaylistSongById(playlist_id);
+		List<Song> song = new ArrayList<Song>();
+		
+		for(int i = 0; i < playlistSong.size(); i++){
+			song.add(playlistSong.get(i).getSong());
+		}
+		
+		model.addAttribute("SongInPlaylist", song);
+		
+		return "musicplayer/playlist/showSongInPlaylist";
 	}
-
 }
