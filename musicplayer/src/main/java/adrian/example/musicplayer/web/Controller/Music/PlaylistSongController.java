0535e5894b9b5959f395df0a1081ecd9412ad9bb
Adrian Durejko <dinor913@gmail.com>
2015-01-05 19:03:19 +0100

    In method "saveSongToPlaylist" add to returned response name of "Song" and "Playlist".
    Add method "playSongFromPlaylist", this method play song with given
    "song_id";

--- musicplayer/src/main/java/adrian/example/musicplayer/web/Controller/Music/PlaylistSongController.java
+++ musicplayer/src/main/java/adrian/example/musicplayer/web/Controller/Music/PlaylistSongController.java
@@ -3,7 +3,7 @@ package adrian.example.musicplayer.web.Controller.Music;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.transaction.Transactional;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,16 +16,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import adrian.example.musicplayer.model.Music.Song;
+import adrian.example.musicplayer.model.Music.playlist.Playlist;
 import adrian.example.musicplayer.model.Music.playlist.PlaylistSong;
+import adrian.example.musicplayer.service.music.MusicService;
 import adrian.example.musicplayer.service.music.PlayListSongService;
+import adrian.example.musicplayer.service.music.PlaylistService;
 
 @Controller
 @RequestMapping(value = "/playlist")
-@SessionAttributes("user")
+@SessionAttributes({"user"})
 public class PlaylistSongController {
 	
 	@Autowired
 	PlayListSongService playListSongService;
+	
+	@Autowired
+	MusicService musicService;
+	
+	@Autowired
+	PlaylistService playlistService;
 	
 	@RequestMapping(value  = "/saveSongToPlaylist", method = RequestMethod.GET)
 	@ResponseBody
@@ -34,24 +43,38 @@ public class PlaylistSongController {
 			@RequestParam(value = "playlist_id", required = true) int playlist_id){
 		
 		this.playListSongService.savePlaylistSong(song_id, playlist_id);
-		return "succed";
+        Song song = this.musicService.loadSongById(song_id);		
+        Playlist playlist = this.playlistService.getPlaylistById(playlist_id);
+        
+		return "succed save song: " + song.getName() + " to playlist: " + playlist.getName();
 	}
 	
 	@RequestMapping(value = "/showPlaylistSong/{playlist_id}", method = RequestMethod.GET)
-	public String getSongPlaylist(
-			@PathVariable(value = "playlist_id") int  playlist_id,
-			Model model){
+	public String getSongPlaylist(@PathVariable(value = "playlist_id") int  playlist_id,
+			HttpSession session){
 
 		List<PlaylistSong> playlistSong = 
-				(List<PlaylistSong>) this.playListSongService.getPlaylistSongById(playlist_id);
+				(List<PlaylistSong>) this.playListSongService.getListOfPlaylistSongById(playlist_id);
 		List<Song> song = new ArrayList<Song>();
 		
 		for(int i = 0; i < playlistSong.size(); i++){
 			song.add(playlistSong.get(i).getSong());
 		}
 		
-		model.addAttribute("SongInPlaylist", song);
+		session.setAttribute("songInPlaylist", song);
 		
 		return "musicplayer/playlist/showSongInPlaylist";
+	}
+	
+	@RequestMapping(value = "/playSongFromPlaylist", method = RequestMethod.GET)
+	public String playSongFromPlaylist(
+			@RequestParam(value = "song_id", required = true) int song_id,
+			Model model){
+		
+		Song song = this.musicService.loadSongById(song_id);
+		model.addAttribute("location",song.getFilename());
+		model.addAttribute("SongName", song.getName());
+		
+		return  "musicplayer/playlist/showSongInPlaylist";
 	}
 }
