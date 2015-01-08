e273d67346bd57fb44ef1ab8b1d75f5bca6005fb
Adrian Durejko <dinor913@gmail.com>
2015-01-08 01:13:28 +0100

    Add readOnly to Transactional who are not save data to database.

--- musicplayer/src/main/java/adrian/example/musicplayer/service/music/MusicServiceImpl.java
+++ musicplayer/src/main/java/adrian/example/musicplayer/service/music/MusicServiceImpl.java
@@ -2,10 +2,9 @@ package adrian.example.musicplayer.service.music;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import adrian.example.musicplayer.dao.music.MusicPlayerDao;
 import adrian.example.musicplayer.model.Music.Song;
@@ -17,27 +16,27 @@ public class MusicServiceImpl implements MusicService {
 	MusicPlayerDao musicPlayerDao;
 
 	@Override
-	@Transactional
+	@Transactional(readOnly = true)
 	public List<Song> getSongByGenre(String genre_name) {
 		return this.musicPlayerDao.getSongByGenre(genre_name);
 	}
 	
 	@Override
-	@Transactional
+	@Transactional(readOnly = true)
 	public List<Song> getSongByAlbum(String album_name) {
 		return this.musicPlayerDao.getSongByAlbum(album_name);
 	}
 
 	@Override
-	@Transactional
+	@Transactional(readOnly = true)
 	public List<Song> getSongByArtist(String artist_name) {
 		return this.musicPlayerDao.getSongByArtist(artist_name);
 	}
 	
 	@Override
-	@Transactional
-	public Song loadSongById(int id) {
-		return this.musicPlayerDao.loadSongById(id);
+	@Transactional(readOnly = true)
+	public Song getSongById(int id) {
+		return this.musicPlayerDao.getSongById(id);
 	}
 	
 
