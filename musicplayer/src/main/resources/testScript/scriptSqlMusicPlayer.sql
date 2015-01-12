c590f6b4282cc097235df3c5d5d9624ecada3eac
Adrian Durejko <dinor913@gmail.com>
2015-01-12 16:30:12 +0100

    Add script needed to Test.

--- musicplayer/src/main/resources/testScript/scriptSqlMusicPlayer.sql
+++ musicplayer/src/main/resources/testScript/scriptSqlMusicPlayer.sql
@@ -1,8 +1,32 @@
 INSERT INTO user VALUES (DEFAULT, 12345, 'TestAddress', 'TestEmail@Test.com', 0, 'TestFirstName', 'TestLogin', '$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK');
 
+INSERT INTO user VALUES (DEFAULT, 12342, 'TestAddress2', 'TestEmail1@Test.com', 0, 'TestFirstName2', 'TestLogin2', '$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK');
+
+INSERT INTO user VALUES (DEFAULT, 12343, 'TestAddress3', 'TestEmail1@Test.com', 0, 'TestFirstName3', 'TestLogin3', '$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK');
+
+INSERT INTO user VALUES (DEFAULT, 12344, 'TestAddress4', 'TestEmail1@Test.com', 0, 'TestFirstName4', 'TestLogin4', '$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK');
+
+INSERT INTO user VALUES (DEFAULT, 12341, 'TestAddress5', 'TestEmail1@Test.com', 0, 'TestFirstName5', 'TestLogin5', '$2a$10$hhBN0WC0n.uVnFk5hqfTsOu4bnJ2UlCogeqELgib.jXqMj4dC6MsK');
+
 INSERT INTO user_information VALUES (1, 23, 'Football, Video Game, Travel', 'JAVA, JAVAEE', 'yes', 'TestSurname');
 
-INSERT INTO user_role VALUES (1, 'ROLE_USER', 'TestLogin', 1);
+INSERT INTO user_information VALUES (2, 22, 'Football, Video Game, Travel', 'JAVA, JAVAEE', 'yes', 'TestSurname2');
+
+INSERT INTO user_information VALUES (3, 24, 'Football, Video Game, Travel', 'JAVA, JAVAEE', 'yes', 'TestSurname3');
+
+INSERT INTO user_information VALUES (4, 25, 'Football, Video Game, Travel', 'JAVA, JAVAEE', 'yes', 'TestSurname4');
+
+INSERT INTO user_information VALUES (5, 26, 'Football, Video Game, Travel', 'JAVA, JAVAEE', 'yes', 'TestSurname5');
+
+INSERT INTO user_role VALUES (DEFAULT, 'ROLE_USER', 'TestLogin', 1);
+
+INSERT INTO user_role VALUES (DEFAULT, 'ROLE_USER', 'TestLogin2', 2);
+
+INSERT INTO user_role VALUES (DEFAULT, 'ROLE_USER', 'TestLogi3', 3);
+
+INSERT INTO user_role VALUES (DEFAULT, 'ROLE_USER', 'TestLogin4', 4);
+
+INSERT INTO user_role VALUES (DEFAULT, 'ROLE_USER', 'TestLogin5', 5);
 
 INSERT INTO ALBUM VALUES (DEFAULT, 'TestAlbum', '/TestLocation', 2000);
 
@@ -27,4 +51,15 @@ INSERT INTO playlist_song VALUES(DEFAULT, 1, 1);
 INSERT INTO playlist_song VALUES(DEFAULT, 1, 2);
 
 INSERT INTO playlist_song VALUES(DEFAULT, 1, 3);
+
+INSERT INTO rate_song VALUES(DEFAULT, 5, 1, 1);
+
+INSERT INTO rate_song VALUES(DEFAULT, 4, 1, 2);
+
+INSERT INTO rate_song VALUES(DEFAULT, 3, 1, 3);
+
+INSERT INTO rate_song VALUES(DEFAULT, 2, 1, 4);
+
+INSERT INTO rate_song VALUES(DEFAULT, 5, 2, 1);
+
 
