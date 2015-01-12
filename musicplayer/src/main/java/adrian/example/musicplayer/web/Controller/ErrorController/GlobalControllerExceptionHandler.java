687138728fd47c99bbc7c5a51cc753141568ec69
Adrian Durejko <dinor913@gmail.com>
2015-01-12 16:18:43 +0100

    Change return  statement in method "hadleConfilct"  to "ResponseEntity" with specific message and error.
    Add method "customHandler", this method iss call whenever we catch
    exception "NullPointerException.class", in reposonse we return
    "ResponseEntity" with specific message and error.

--- musicplayer/src/main/java/adrian/example/musicplayer/web/Controller/ErrorController/GlobalControllerExceptionHandler.java
+++ musicplayer/src/main/java/adrian/example/musicplayer/web/Controller/ErrorController/GlobalControllerExceptionHandler.java
@@ -6,14 +6,26 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
 
 	@ExceptionHandler(ConstraintViolationException.class)
-	public void hadleConfilct(HttpServletResponse response) throws IOException{
-       response.sendError(409);		
+	public ResponseEntity<String> handleConfilct(){
+	        return new ResponseEntity<String>(
+	                "This Song is already in this playlist",
+	                HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+	
+	@ExceptionHandler(NullPointerException.class)
+	 ResponseEntity<String> customHandler(Exception ex) {
+        return new ResponseEntity<String>(
+                "Sorry, you must rating first",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+      } 
 }
