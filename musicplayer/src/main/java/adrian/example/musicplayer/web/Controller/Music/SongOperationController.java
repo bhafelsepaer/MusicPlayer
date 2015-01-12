3315025e4d6b431bb3408ccef52f2a80d28926d1
Adrian Durejko <dinor913@gmail.com>
2015-01-12 16:29:13 +0100

    Remove Session Attrbiute "user".
    Add method "showRatingSongbyUser" and "showAverageSongRating".

--- musicplayer/src/main/java/adrian/example/musicplayer/web/Controller/Music/SongOperationController.java
+++ musicplayer/src/main/java/adrian/example/musicplayer/web/Controller/Music/SongOperationController.java
@@ -6,21 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import adrian.example.musicplayer.model.Music.Song;
 import adrian.example.musicplayer.service.music.MusicService;
 import adrian.example.musicplayer.service.music.RateSongService;
 
 @Controller
-@RequestMapping("/song")
-@SessionAttributes("user")
+@RequestMapping(value = "/song")
 public class SongOperationController {
      
 	@Autowired
 	RateSongService rateSongService;
 	
-	@Autowired MusicService musicService;
+	@Autowired 
+	MusicService musicService;
 	
 	@RequestMapping(value = "/rateSongByUser", method = RequestMethod.GET)
 	@ResponseBody
@@ -30,9 +29,39 @@ public class SongOperationController {
 			@RequestParam(value = "ratingSong") int ratingSong){
 		
 		this.rateSongService.saveRatingSong(user_id, song_id, ratingSong);
-		
 		Song song = this.musicService.getSongById(song_id);
 	
 		return "succed rate Song " + song.getName();
 	}
+	
+	@RequestMapping(value = "/showRatingSongbyUser", method = RequestMethod.GET)   
+	@ResponseBody
+	public double showRatingSongbyUser(
+			@RequestParam(value = "user_id") int user_id,
+		    @RequestParam(value="song_id") int song_id) {
+		
+		double raiting_song = this.rateSongService.
+				                getRateSongByUser(user_id, song_id);
+		  
+		return raiting_song;
+	}
+	
+	@RequestMapping(value = "/showAverageRatingSong", method = RequestMethod.GET)
+	@ResponseBody
+	public double showAverageSongRating(@RequestParam(value = "song_id") int song_id){
+		
+		double ratingSong = this.rateSongService.getAverageSongRating(song_id);
+		return ratingSong;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
