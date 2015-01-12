217d654b1e4d57ca7fdb5ed2d8451926fb32763d
Adrian Durejko <dinor913@gmail.com>
2015-01-12 16:07:54 +0100

    Add method "getRateSongByUser" and "getRateingSongBelongToSong"

--- musicplayer/src/main/java/adrian/example/musicplayer/dao/music/RateSongDao.java
+++ musicplayer/src/main/java/adrian/example/musicplayer/dao/music/RateSongDao.java
@@ -1,5 +1,7 @@
 package adrian.example.musicplayer.dao.music;
 
+import java.util.List;
+
 import adrian.example.musicplayer.model.Music.RateSong;
 
 public interface RateSongDao {
@@ -7,4 +9,8 @@ public interface RateSongDao {
 	void saveRatingSong(int user_id, int song_id, int ratingSong);
 	
 	RateSong getRateSongById(int rating_id);
+	
+	double getRateSongByUser(int user_id, int song_id);
+	
+	List<Double> getRateingSongBelongToSong(int song_id);
 }
