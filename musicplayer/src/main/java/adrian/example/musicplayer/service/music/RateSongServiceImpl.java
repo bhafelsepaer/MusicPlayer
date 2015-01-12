3263c9bbc7eaad1cd770853ea3ddefc537eb6d45
Adrian Durejko <dinor913@gmail.com>
2015-01-12 16:11:47 +0100

    Implements method "getRateSongByUser" and "getAverageSongRating".

--- musicplayer/src/main/java/adrian/example/musicplayer/service/music/RateSongServiceImpl.java
+++ musicplayer/src/main/java/adrian/example/musicplayer/service/music/RateSongServiceImpl.java
@@ -1,5 +1,7 @@
 package adrian.example.musicplayer.service.music;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,9 +23,31 @@ public class RateSongServiceImpl implements RateSongService {
 	}
 
 	@Override
+	@Transactional
 	public RateSong getRateSongById(int rating_id) {
 		return this.rateSongDao.
 				    getRateSongById(rating_id);
 	}
 
+	@Override
+	@Transactional
+	public double getRateSongByUser(int user_id, int song_id) {
+		return this.rateSongDao
+				.getRateSongByUser(user_id, song_id);
+	}
+
+	@Override
+	@Transactional
+	public double getAverageSongRating(int song_id) {
+		List<Double> listofRatingSong = 
+				this.rateSongDao.getRateingSongBelongToSong(song_id);
+		
+		double ratingAverageSong = 0;
+		
+		 for(double rateSong : listofRatingSong){
+	        	ratingAverageSong += rateSong;
+	     }
+		 
+		return ratingAverageSong / listofRatingSong.size();
+	}
 }
